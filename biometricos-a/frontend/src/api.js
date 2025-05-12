d5008099c6cc499d6a712a3377ae06690ce7398e
Davidb2303 <dballesteros2303@gmail.com>
2025-05-12 15:52:42 -0500

    Deploy local

--- biometricos-a/frontend/src/api.js
+++ biometricos-a/frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: '/api/', 
+    baseURL: 'http://localhost:8000', 
     timeout: 5000, 
     headers: {
         'Content-Type': 'application/json',
